4adbb9e8fe1f614671208b756b5839f79248aae6
hadardv <hadardv14@gmail.com>
2026-04-18 16:18:44 +0300

    fix port problem

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3001',
     credentials: true,
 }));
 
